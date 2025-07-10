cadce5aaa1996c33f153195127ea2b39a31b290a
Sam-Dev45 <samuel.mycloud@gmail.com>
2025-07-10 15:25:24 +0530

    Your backup10072025

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -8,13 +8,13 @@ const EmployeeController = {
 
   create: async (req, res) => {
     const data = await EmployeeService.create(req.body);
-    res.json({ message: "Created", data });
+    res.json(data );
   },
 
   update: async (req, res) => {
     const data = await EmployeeService.update(req.params.id, req.body);
     if (!data) return res.status(404).send({ message: "Not found" });
-    res.json({ message: "Updated", data });
+    res.json(data);
   },
 
   delete: async (req, res) => {
